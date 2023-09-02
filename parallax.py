3abce6ace73dc58dc97e5907ddf70ed1b6533dd5
Niall <niall.j.miller@gmail.com>
2023-09-02 11:27:35 +0100

    weeewoooooo

--- parallax.py
+++ parallax.py
@@ -10,44 +10,52 @@ from torch import Tensor  # Tensor class from PyTorch
 #from torch.nn import Linear, Sigmoid, Module, ReLU, HuberLoss, BatchNorm1d  # Neural network layers and loss functions
 import torch.nn as nn
 from torch.nn.init import xavier_uniform_  # Xavier uniform weight initialization
-from torch.optim import Adam  # Adam optimizer
+import torch.optim as optim
 from torch.optim.lr_scheduler import ExponentialLR  # Learning rate scheduler
 from torch.utils.data import Dataset, DataLoader, random_split  # PyTorch data loading utilities
 import torch.nn.functional as F
 
 from sklearn.preprocessing import StandardScaler  # Data normalization
-from sklearn.metrics import mean_squared_error  # Evaluating model performance
-
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error # Evaluating model performance
 import matplotlib.pyplot as plt  # Plotting
 from matplotlib import rcParams  # Customizing plot parameters
 
 from tqdm import tqdm  # Creating progress bars
 
-
 class MLP(nn.Module):
     def __init__(self, n_inputs: int):
         super(MLP, self).__init__()
-        self.hidden1 = nn.Linear(n_inputs, 256)
-        self.bn1 = nn.BatchNorm1d(256)
+        self.hidden1 = nn.Linear(n_inputs, 512)
+        self.bn1 = nn.BatchNorm1d(512)
         self.act1 = nn.ReLU()
         self.dropout1 = nn.Dropout(0.2)
         
-        self.hidden2 = nn.Linear(256, 128)
-        self.bn2 = nn.BatchNorm1d(128)
+        self.hidden2 = nn.Linear(512, 256)
+        self.bn2 = nn.BatchNorm1d(256)
         self.act2 = nn.ReLU()
         self.dropout2 = nn.Dropout(0.2)
         
-        self.hidden3 = nn.Linear(128, 64)
-        self.bn3 = nn.BatchNorm1d(64)
+        self.hidden3 = nn.Linear(256, 128)
+        self.bn3 = nn.BatchNorm1d(128)
         self.act3 = nn.ReLU()
         self.dropout3 = nn.Dropout(0.2)
         
-        self.hidden4 = nn.Linear(64, 32)
-        self.bn4 = nn.BatchNorm1d(32)
+        self.hidden4 = nn.Linear(128, 64)
+        self.bn4 = nn.BatchNorm1d(64)
         self.act4 = nn.ReLU()
         self.dropout4 = nn.Dropout(0.2)
         
-        self.hidden5 = nn.Linear(32, 1)
+        self.hidden5 = nn.Linear(64, 32)
+        self.bn5 = nn.BatchNorm1d(32)
+        self.act5 = nn.ReLU()
+        self.dropout5 = nn.Dropout(0.2)
+        
+        self.hidden6 = nn.Linear(32, 16)
+        self.bn6 = nn.BatchNorm1d(16)
+        self.act6 = nn.ReLU()
+        self.dropout6 = nn.Dropout(0.2)
+        
+        self.hidden7 = nn.Linear(16, 1)
 
         self._initialize_weights()
 
@@ -80,8 +88,17 @@ class MLP(nn.Module):
         x = self.dropout4(x)
         
         x = self.hidden5(x)
+        x = self.bn5(x)
+        x = self.act5(x)
+        x = self.dropout5(x)
+        
+        x = self.hidden6(x)
+        x = self.bn6(x)
+        x = self.act6(x)
+        x = self.dropout6(x)
+        
+        x = self.hidden7(x)
         return x
-
 
 class CSVDataset(Dataset):
     def __init__(self, path: str, x_filter: list, x_importance: list, y_filter: list):
@@ -172,27 +189,32 @@ def weigh_features(scaler, importance):
 
     return scaler_weighted
 
+def train_model(train_dl: DataLoader, test_dl: DataLoader, model: nn.Module, epochs: int, learn_rate: float, output_dir: str, gamma: float):
 
-def train_model(train_dl: DataLoader, test_dl: DataLoader, model: nn.Module,
-                epochs: int, learn_rate: float, output_dir: str, gamma: float):
-    criterion = nn.HuberLoss()
-    opt = Adam(model.parameters(), lr=learn_rate)
-    scheduler = ExponentialLR(opt, gamma=gamma)
+    # During training/validation, calculate additional evaluation metrics
+    def evaluate_metrics(y_true, y_pred):
+        r2 = r2_score(y_true, y_pred)
+        mse = mean_squared_error(y_true, y_pred)
+        mae = mean_absolute_error(y_true, y_pred)
+        return r2, mse, mae
+
     min_delta = 0.0001
-    tolerance = 10
+    tolerance = 3
     counter = 0
-    train_stats = pd.DataFrame(columns=['epoch', 'learn_rate', 'loss', 'delta_loss', 'counter', 'val_loss'])
-    i = 0
+    criterion = nn.SmoothL1Loss()
+    optimizer = optim.Adam(model.parameters(), lr=learn_rate)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=gamma, patience=tolerance, verbose=True)
+    train_stats = pd.DataFrame(columns=['epoch', 'learn_rate', 'loss', 'delta_loss', 'counter', 'val_loss', 'r2_score', 'mse', 'mae'])
     mse = np.nan
 
     for epoch in range(epochs):
         model.train()
         for train_features, train_labels in train_dl:
-            opt.zero_grad()
+            optimizer.zero_grad()
             yhat = model(train_features)
             loss = criterion(yhat, train_labels)
             loss.backward()
-            opt.step()
+            optimizer.step()
 
         model.eval()
         with torch.no_grad():
@@ -201,33 +223,30 @@ def train_model(train_dl: DataLoader, test_dl: DataLoader, model: nn.Module,
                 val_loss = criterion(prediction_val, test_labels)
                 loss_delta = val_loss - loss
 
-        my_lr = scheduler.get_last_lr()
-        train_stats.loc[i + (epoch * len(train_dl))] = [epoch, float(my_lr[0]), float(loss.detach().numpy()),
-                                                        float(loss_delta.detach().numpy()), counter,
-                                                        val_loss.detach().numpy()]
 
         if not epoch % 2:
-            scheduler.step()
+            my_lr = optimizer.param_groups[0]['lr']
+            r2,mse,mae = evaluate_metrics(train_labels.detach().numpy() ,yhat.detach().numpy())
+            train_stats.loc[epoch] = [epoch, float(my_lr), float(loss.detach().numpy()),
+                                      float(loss_delta.detach().numpy()), counter,
+                                      float(val_loss.detach().numpy()), r2, mse, mae]
+            scheduler.step(val_loss)
 
         if not epoch % 16:
-            print('Epoch:', epoch, 'Learning rate:', float(my_lr[0]), 'Val Loss:', float(val_loss), 'Loss:',
+            print('Epoch:', epoch, 'Learning rate:', float(my_lr), 'Val Loss:', float(val_loss), 'Loss:',
                   float(loss), 'Delta Loss:', float(loss_delta), 'ES counter:', counter)
 
             if not epoch % 1024 and epoch > 1:
-                mse = evaluate_model(test_dl, model)
-                print('MSE: %.3f, RMSE: %.3f' % (mse, np.sqrt(mse)))
                 plots(train_stats, output_dir, mse)
 
-        if epoch > 1024:
-            if loss_delta > min_delta:
-                counter += 1
-            if counter > 10:
-                if counter >= tolerance:
-                    print('Epoch:', epoch, 'Learning rate:', float(my_lr[0]), 'Val Loss:', float(val_loss),
-                          'Loss:', float(loss), 'Delta Loss:', float(loss_delta), 'ES counter:', counter)
-                    print("STOPPING at epoch:", epoch)
-                    plots(train_stats, output_dir, mse)
-                    return
+        if epoch > 1024 and loss_delta > min_delta:
+            counter += 1
+            if counter > 10 and counter >= tolerance:
+                print('Epoch:', epoch, 'Learning rate:', float(my_lr), 'Val Loss:', float(val_loss),
+                      'Loss:', float(loss), 'Delta Loss:', float(loss_delta), 'ES counter:', counter)
+                print("STOPPING at epoch:", epoch)
+                plots(train_stats, output_dir, mse)
+                return
 
     return
 
@@ -325,6 +344,39 @@ def plots(train_stats: pd.DataFrame, output_dir: str, mse: float):
         plt.close()
     except Exception as e:
         print(f'Did not plot, because {repr(e)}')
+    try:
+        plt.clf()
+        fig, ax = plt.subplots()
+        ax.axhline(y=0, color='r', linestyle='-')
+        ax.plot(train_stats['epoch'], train_stats['mse'], 'k', alpha=0.9, ms=2)
+        ax.set(xlabel='Epoch', ylabel='MSE', xscale='log', yscale='log')
+        fig.tight_layout()
+        plt.savefig(output_dir + '/log_mse.jpg', bbox_inches='tight')
+        plt.close()
+    except Exception as e:
+        print(f'Did not plot, because {repr(e)}')
+    try:
+        plt.clf()
+        fig, ax = plt.subplots()
+        ax.axhline(y=0, color='r', linestyle='-')
+        ax.plot(train_stats['epoch'], train_stats['mae'], 'k', alpha=0.9, ms=2)
+        ax.set(xlabel='Epoch', ylabel='MAE', xscale='log', yscale='log')
+        fig.tight_layout()
+        plt.savefig(output_dir + '/log_mae.jpg', bbox_inches='tight')
+        plt.close()
+    except Exception as e:
+        print(f'Did not plot, because {repr(e)}')
+    try:
+        plt.clf()
+        fig, ax = plt.subplots()
+        ax.axhline(y=0, color='r', linestyle='-')
+        ax.plot(train_stats['epoch'], train_stats['r2_score'], 'k', alpha=0.9, ms=2)
+        ax.set(xlabel='Epoch', ylabel='R2', xscale='log', yscale='log')
+        fig.tight_layout()
+        plt.savefig(output_dir + '/log_r2.jpg', bbox_inches='tight')
+        plt.close()
+    except Exception as e:
+        print(f'Did not plot, because {repr(e)}')
     return
 
 
@@ -397,25 +449,86 @@ def predict_dist(model: nn.Module, data_fp: str, data_verif_fp: str, output_dir:
     df_train['parallax_NN'] = inversed_y
 
     print('Creating plots')
+    # Filter the data for this on this side of the bulge
+    df_train_filtered = df_train[df_train['parallax_corr'] > 1/8]
+    df_verif_filtered = df_verif[df_verif['parallax_corr'] > 1/8]
+
     plt.clf()
     fig, ax = plt.subplots()
     ax.plot(df_train['parallax_corr'], df_train['parallax_NN'], 'bx', alpha=0.1, ms=2)
     ax.plot(df_verif['parallax_corr'], df_verif['parallax_NN'], 'gx', alpha=0.1, ms=2)
     ax.axline((0, 0), (1, 1), color='k')
-    ax.set(xlabel='True [Parsec]', ylabel='Predicted [Parsec]')
+    ax.set(xlabel='True [mas]', ylabel='Predicted [mas]')
     fig.tight_layout()
     plt.savefig(output_dir + '/pred_true_plx.jpg', bbox_inches='tight')
 
     plt.clf()
     fig, ax = plt.subplots()
+    ax.plot(1/df_train['parallax_corr'], 1/df_train['parallax_NN'], 'bx', alpha=0.1, ms=2)
+    ax.plot(1/df_verif['parallax_corr'], 1/df_verif['parallax_NN'], 'gx', alpha=0.1, ms=2)
+    ax.axline((0, 0), (1, 1), color='k')
+    ax.set(xlabel='True [1/mas]', ylabel='Predicted [1/mas]')
+    fig.tight_layout()
+    plt.savefig(output_dir + '/pred_true_dist.jpg', bbox_inches='tight')
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.plot(df_train_filtered['parallax_corr'], df_train_filtered['parallax_NN'], 'bx', alpha=0.1, ms=2)
+    ax.plot(df_verif_filtered['parallax_corr'], df_verif_filtered['parallax_NN'], 'gx', alpha=0.1, ms=2)
+    ax.axline((0, 0), (1, 1), color='k')
+    ax.set(xlabel='True [mas]', ylabel='Predicted [mas]')
+    fig.tight_layout()
+    plt.savefig(output_dir + '/pred_true_plx_close.jpg', bbox_inches='tight')
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.plot(1/df_train_filtered['parallax_corr'], 1/df_train_filtered['parallax_NN'], 'bx', alpha=0.1, ms=2)
+    ax.plot(1/df_verif_filtered['parallax_corr'], 1/df_verif_filtered['parallax_NN'], 'gx', alpha=0.1, ms=2)
+    ax.axline((0, 0), (1, 1), color='k')
+    ax.set(xlabel='True [1/mas]', ylabel='Predicted [1/mas]')
+    fig.tight_layout()
+    plt.savefig(output_dir + '/pred_true_dist_close.jpg', bbox_inches='tight')
+
+    print('Creating log plots')
+    plt.clf()
+    fig, ax = plt.subplots()
     ax.loglog(df_train['parallax_corr'], df_train['parallax_NN'], 'bx', alpha=0.1, ms=2)
     ax.loglog(df_verif['parallax_corr'], df_verif['parallax_NN'], 'gx', alpha=0.1, ms=2)
     ax.axline((0, 0), (1, 1), color='k')
-    ax.set(xlabel='True [Parsec]', ylabel='Predicted [Parsec]')
+    ax.set(xlabel='True [mas]', ylabel='Predicted [mas]')
     fig.tight_layout()
-    plt.savefig(output_dir + '/pred_true_plx_log.jpg', bbox_inches='tight')
-    print('Plotted predicted parallaxes')
+    plt.savefig(output_dir + '/pred_true_plx.jpg', bbox_inches='tight')
 
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.loglog(1/df_train['parallax_corr'], 1/df_train['parallax_NN'], 'bx', alpha=0.1, ms=2)
+    ax.loglog(1/df_verif['parallax_corr'], 1/df_verif['parallax_NN'], 'gx', alpha=0.1, ms=2)
+    ax.axline((0, 0), (1, 1), color='k')
+    ax.set(xlabel='True [1/mas]', ylabel='Predicted [1/mas]')
+    fig.tight_layout()
+    plt.savefig(output_dir + '/pred_true_dist_log.jpg', bbox_inches='tight')
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.loglog(df_train_filtered['parallax_corr'], df_train_filtered['parallax_NN'], 'bx', alpha=0.1, ms=2)
+    ax.loglog(df_verif_filtered['parallax_corr'], df_verif_filtered['parallax_NN'], 'gx', alpha=0.1, ms=2)
+    ax.axline((0, 0), (1, 1), color='k')
+    ax.set(xlabel='True [mas]', ylabel='Predicted [mas]')
+    fig.tight_layout()
+    plt.savefig(output_dir + '/pred_true_plx_close_log.jpg', bbox_inches='tight')
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.loglog(1/df_train_filtered['parallax_corr'], 1/df_train_filtered['parallax_NN'], 'bx', alpha=0.1, ms=2)
+    ax.loglog(1/df_verif_filtered['parallax_corr'], 1/df_verif_filtered['parallax_NN'], 'gx', alpha=0.1, ms=2)
+    ax.axline((0, 0), (1, 1), color='k')
+    ax.set(xlabel='True [1/mas]', ylabel='Predicted [1/mas]')
+    fig.tight_layout()
+    plt.savefig(output_dir + '/pred_true_dist_close_log.jpg', bbox_inches='tight')
+
+
+    print('Plotted predicted parallaxes')
+    print('Creating colour plots')
     plot_corrs(df_train, output_dir, x_filter, 'trained')
     plot_corrs(df_verif, output_dir, x_filter, 'verif')
     print('Plotted coloured properties')
@@ -453,6 +566,34 @@ def plot_corrs(df: pd.DataFrame, output_dir: str, properties: list, name: str):
     plt.tight_layout()
     plt.savefig(output_dir + '/pred_true_plx_colour_' + str(name) + '_vars.jpg', bbox_inches='tight')
     plt.close()
+
+
+    #log version 
+    f, axs = plt.subplots(4, len(properties) // 4, figsize=(3.75 * len(properties) // 4, 4 * 4))
+    for ax, emergent_property in zip(axs.ravel(), properties):
+        colours = df.loc[:, emergent_property].array[idx]
+        ax: plt.Axes = ax
+
+        if emergent_property == 'parallax':
+            colours = np.clip(colours, -4, 4)
+        if emergent_property == 'pmra':
+            colours = np.clip(colours, -14, 14)
+        if emergent_property == 'pmdec':
+            colours = np.clip(colours, -14, 14)
+        if emergent_property == 'Plx':
+            colours = np.clip(colours, -2, 2)
+        if emergent_property == 'PM_x':
+            colours = np.clip(colours, 0, 20)
+
+        ax.set_title(emergent_property.replace('_', ' '))
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        im = ax.scatter(x, y, edgecolors='black', c=colours, cmap="brg")
+        f.colorbar(im, ax=ax)
+
+    plt.tight_layout()
+    plt.savefig(output_dir + '/pred_true_plx_colour_log_' + str(name) + '_vars.jpg', bbox_inches='tight')
+    plt.close()
     return
 
 
@@ -460,63 +601,60 @@ def main(input_prefix: str = '', output_prefix: str = ''):
     """
     Main Driver function
     """
-    epochs = 4096
+    epochs = 2048
     learn_rate = 0.1
-    learn_rate_gamma = 0.991
+    learn_rate_gamma = 0.9
 
+    data_fp = input_prefix + 'VVV_GAIA_STANDARDS.csv'
+    data_verif_fp = data_fp#input_prefix + 'GAIA_VARS.csv'
 
-    for learn_rate_gamma in [0.99,0.991,0.80,0.85,0.5]:
+    x_filter = ['parallax_corr','phot_bp_rp_excess_factor_corr',
+        'ra','dec','l','b','ecl_lon','ecl_lat',
+        'parallax','pmra','pmdec',
+        'dec_parallax_corr','dec_pmdec_corr','dec_pmra_corr',
+        'parallax_pmdec_corr','parallax_pmra_corr',
+        'pm','pmra_pmdec_corr','ra_dec_corr','radial_velocity',
+        'ra_parallax_corr','ra_pmdec_corr','ra_pmra_corr',
+        'ra_vvv','dec_vvv','l_vvv','b_vvv','parallax_vvv',
+        'pmra_vvv','pmdec_vvv',
+        'bp_g','bp_rp',
+        'g_rp','grvs_mag',
+        'J-K','H-K','Z-K','Y-K']
+        #Z-K and Y-K might have bad data, check weights.
 
-        data_fp = input_prefix + 'VVV_GAIA_STANDARDS.csv'
-        data_verif_fp = data_fp#input_prefix + 'GAIA_VARS.csv'
+    x_importance = [0.6,0.2, 0.4,0.4,0.4,0.4,0.4,0.4,
+	        0.6,0.6,0.6,0.5,0.3,0.3,
+	        0.3,0.3,0.8,0.3,0.3,
+	        0.7,0.3,0.3,0.3,0.7,
+	        0.5,0.5,0.5,1,1,1,0.5,0.5,
+	        0.5,0.4,0.7,0.7,0.4,0.4]
 
-        x_filter = ['parallax_corr','phot_bp_rp_excess_factor_corr',
-	        'ra','dec','l','b','ecl_lon','ecl_lat',
-	        'parallax','pmra','pmdec',
-	        'dec_parallax_corr','dec_pmdec_corr','dec_pmra_corr',
-	        'parallax_pmdec_corr','parallax_pmra_corr',
-	        'pm','pmra_pmdec_corr','ra_dec_corr','radial_velocity',
-	        'ra_parallax_corr','ra_pmdec_corr','ra_pmra_corr',
-	        'ra_vvv','dec_vvv','l_vvv','b_vvv','parallax_vvv',
-	        'pmra_vvv','pmdec_vvv',
-	        'bp_g','bp_rp',
-	        'g_rp','grvs_mag',
-	        'J-K','H-K','Z-K','Y-K']
-            #Z-K and Y-K might have bad data, check weights.
+    y_filter = ['parallax_corr']
+    output_dir = output_prefix + str(epochs) + '_' + str(int(1 / learn_rate)) + '_' + str(int(10000 * learn_rate_gamma)) + '/'
 
-        x_importance = [0.6,0.2, 0.4,0.4,0.4,0.4,0.4,0.4,
-		        0.6,0.6,0.6,0.5,0.3,0.3,
-		        0.3,0.3,0.8,0.3,0.3,
-		        0.7,0.3,0.3,0.3,0.7,
-		        0.5,0.5,0.5,1,1,1,0.5,0.5,
-		        0.5,0.4,0.7,0.7,0.4,0.4]
+    torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        y_filter = ['parallax_corr']
-        output_dir = output_prefix + str(epochs) + '_' + str(int(1 / learn_rate)) + '_' + str(int(10000 * learn_rate_gamma)) + '/'
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
 
-        torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_dl, test_dl, vector_len = prepare_data(data_fp, x_filter, x_importance, y_filter)
+    print('Data prepared')
 
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        print(device)
+    model = MLP(vector_len)
+    print('Model compiled')
 
-        train_dl, test_dl, vector_len = prepare_data(data_fp, x_filter, x_importance, y_filter)
-        print('Data prepared')
+    if True:#not os.path.exists(output_dir + 'model.pt'):
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        train_model(train_dl, test_dl, model, epochs, learn_rate, output_dir, learn_rate_gamma)
+        torch.save(model.state_dict(), output_dir + 'model.pt')
+        print('Model trained')
+    else:
+        print('Loading model from here : ', output_dir + 'model.pt')
+        model.load_state_dict(torch.load(output_dir + 'model.pt'))
 
-        model = MLP(vector_len)
-        print('Model compiled')
-
-        if True:#not os.path.exists(output_dir + 'model.pt'):
-            if not os.path.exists(output_dir):
-                os.mkdir(output_dir)
-            train_model(train_dl, test_dl, model, epochs, learn_rate, output_dir, learn_rate_gamma)
-            torch.save(model.state_dict(), output_dir + 'model.pt')
-            print('Model trained')
-        else:
-            print('Loading model from here : ', output_dir + 'model.pt')
-            model.load_state_dict(torch.load(output_dir + 'model.pt'))
-
-        print('Predicting distances')
-        predict_dist(model, data_fp, data_verif_fp, output_dir, x_filter, x_importance,  y_filter)
+    print('Predicting distances')
+    predict_dist(model, data_fp, data_verif_fp, output_dir, x_filter, x_importance,  y_filter)
     return
 
 
